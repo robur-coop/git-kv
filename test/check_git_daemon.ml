8057f9ad32d95982f2f66b97bdb6f9e98122edfd
Calascibetta Romain <romain.calascibetta@gmail.com>
2025-06-17 15:39:57 +0200

    Remove check_git_daemon.ml

--- test/check_git_daemon.ml
+++
@@ -1,6 +0,0 @@
-let () =
-  let git_daemon = Bos.Cmd.(v "git" % "daemon" % "--help") in
-
-  match Bos.OS.Cmd.run_status ~quiet:true git_daemon with
-  | Ok (`Exited 0) -> print_endline "(true)"
-  | _ -> print_endline "false"
